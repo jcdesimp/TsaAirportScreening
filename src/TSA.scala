e3a9737eca3b1e8c4c97e92f29466c7beb6a29f7
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-11-24 13:37:52 -0500

    Initial Commit

---
+++ src/TSA.scala
@@ -0,0 +1,9 @@
+/**
+ * Created by jcdesimp on 11/24/14.
+ * Main file with main method for running program
+ */
+object TSA {
+  def main (args: Array[String]) {
+    println("Hello World!")
+  }
+}
